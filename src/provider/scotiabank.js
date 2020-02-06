6542ffd7347d20e43a1b1144a868475689574a54
Alexandre Beaulieu <alex@segfault.me>
2020-02-05 23:36:32 -0500

    feat: Added initial scotiabank support.

--- src/provider/scotiabank.js
+++ src/provider/scotiabank.js
@@ -1,14 +1,41 @@
+import { MONTHS, extractTable } from '../core/utils.js';
+
+function parseDate(s) {
+    let [month, day, year] = s.toLowerCase()
+        .replace('.', '')
+        .replace(',', '')
+        .split(' ');
+
+    year = parseInt(year);
+    month = MONTHS[month] - 1;
+    day = parseInt(day);
+    if (!year || !month || !day) return null;
+
+    return new Date(year, month, day);
+}
+
 function scotiabank() {
-    console.log('hello scotia');
-    const eDetails = document.getElementById('accDetailsPanelTransaction');
-    if (!eDetails) return null;
+    const posted = document.getElementById('accDetailsPanelTransaction');
+    if (!posted) return [];
+
     // TODO: Support Pending Transactions too? Will require extra field on transactions.
     // const ePending = document.getElementById('pending_form');
     // const pending = extractTable(ePending);
 
-    const details = extractTable(eDetails);
-    console.log(details);
-    return [];
+    const account = document.getElementsByTagName('h1')[1].innerText;
+
+    const stmt = extractTable(posted);
+    let out = [];
+    for (let r of stmt.rows) {
+        const date = parseDate(r[0]);
+        if (!date) continue; // Not a transaction.
+
+        const details = r[2];
+        const amount = r[3] == "" ? r[4] : r[3]
+        const direction = r[3] == "" ? 1 : -1;
+        out.push({ date, account, details, amount, direction });
+    }
+    return out;
 }
 
 export const provider = {
