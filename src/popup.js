c74b51fdab61754e94780374b8a45d11a679c950
Alexandre Beaulieu <alex@segfault.me>
2020-02-25 19:40:01 -0500

    feat: Added popup functionality.

--- src/popup.js
+++ src/popup.js
@@ -1,11 +1,39 @@
 import Vue from 'vue';
 import Popup from './popup.vue';
 
-new Vue({
+let popup = new Vue({
     el: '#popup',
     render: function(createElement) {
         return createElement(
             Popup
         )
+    },
+    data: {
+        accounts: [],
+        ext: browser.runtime.connect({name: 'ledgerize.popup'}),
     }
 })
+
+// account: { id, name, selected }
+function onMessage(m) {
+    console.debug('Message from extension', m);
+    switch (m.type) {
+        case 'popup.set': {
+            popup.accounts = m.data;
+            break;
+        }
+        case 'popup.add': {
+            popup.accounts.push({ id: m.data.id, ...m.data });
+            break;
+        }
+        case 'popup.rem': {
+            popup.accounts = popup.accounts.filter(a => a.id != m.data);
+            break;
+        }
+        default: console.warn('Unknown message', m);
+    }
+}
+
+popup.ext.onMessage.addListener(onMessage);
+popup.ext.onDisconnect.addListener(x => popup.ext.onMessage.removeHandler(onMessage));
+
--- src/popup.vue
+++ src/popup.vue
@@ -1,44 +1,41 @@
 <template>
   <div id="popup-page">
     <h1>Popup View</h1>
+    <ul v-if="accounts.length">
+      <li v-for="acc in accounts">
+        {{acc.account}} <button v-on:click="extract(acc.id)">Extract</button>
+      </li>
+    </ul>
+    <div v-else>
+      Nothing to see here.
+    </div>
 
     <hr>
-    <button v-on:click="extract">Extract Selected</button>
+    <button v-on:click="extract(null)">Extract All</button>
   </div>
 </template>
 
 <script>
-const accounts = new Map();
-
-const ext = browser.runtime.connect({name: 'ledgerize.popup'});
-
-function onMessage(m) {
-    console.debug('Message from extension', m);
-    switch (m.type) {
-        case 'popup.set': {
-            accounts.clear();
-            m.data.forEach(a => accounts.set(a.id, a.account));
-            break;
-        }
-        case 'popup.add': accounts.set(m.data.id, m.data.account); break;
-        case 'popup.rem': accounts.del(m.data);                    break;
-        default: console.warn('Unknown message', m);
-    }
-}
-ext.onMessage.addListener(onMessage);
-ext.onDisconnect.addListener(x => ext.onMessage.removeHandler(onMessage));
-
+// So, this is pretty hacky, but I haven't found a better way to do this:
+// Since we're using vue-loader, the Vue.app needs a pre-compiled template.
+// I couldn't find a way to instantiate the Vue.app from vue-loader and thus
+// The app is spawned in `popup.js` with the only  task of creating the root
+// component (this file). This component is effectively the top level Vue but
+// it seems like binding props to it without the template syntax is painful,
+// so instead, it uses a computed property that retrieves the accounts from the
+// app's static data. This lets the account list be observable without
+// having to manually control the updates. 
 export default {
     name: 'Popup',
-    data: () => {
-        return { accounts };
-    },
     methods: {
-        extract: function() {
-            console.debug('Extracting', this.accounts);
-            // FIXME: Allow user to select which accounts to extract.
-            ext.postMessage({type: 'extract', data: Array.from(this.accounts.keys())});
+        extract: function(id) {
+            this.$parent.ext.postMessage({type: 'extract', data: id ? [id] : Array.from(this.accounts.map(a => a.id))});
         }
-    }
+    },
+    computed: {
+        accounts: function() {
+            return this.$parent.accounts;
+        }
+    },
 }
 </script>
