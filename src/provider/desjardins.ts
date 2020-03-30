05f8eebf142d8b6945360d896ec38595a52ea060
Alexandre Beaulieu <alex@segfault.me>
2020-03-29 20:33:26 -0400

    cleanup: Desjardins provider comment style.

--- src/provider/desjardins.ts
+++ src/provider/desjardins.ts
@@ -28,24 +28,20 @@ export class Desjardins implements IProvider {
         this.version = '0.1';
     }
 
-    /**
-    * Called by ledgerize when the user has requested transactions to be
-    * extracted.
-    */
+    /// Called by ledgerize when the user has requested transactions to be
+    /// extracted.
     extract(account: string): Array<Transaction> {
-        /*
-        Desjardins has two tables:
-            1. The regular statement that shows the last 31 days
-            2. A custom search query results.
+        // Desjardins has two tables:
+        //     1. The regular statement that shows the last 31 days
+        //     2. A custom search query results.
 
-        When the custom query result table exists, prefer it over the
-        regular statement as the user has most likely explicitly made
-        this search and is expecting this to be exported.
+        // When the custom query result table exists, prefer it over the
+        // regular statement as the user has most likely explicitly made
+        // this search and is expecting this to be exported.
 
-        Currently, only case 1 is supported. Case 2 is loaded in an
-        iframe and will require some tinkering to get working properly.
-        This can be done later.
-        */
+        // Currently, only case 1 is supported. Case 2 is loaded in an
+        // iframe and will require some tinkering to get working properly.
+        // This can be done later.
         let ops = document.getElementById('dernieresoperations');
         // let custom =  document.getElementById('zone') // TODO
 
@@ -58,26 +54,24 @@ export class Desjardins implements IProvider {
         for (let i = 1; i < txs.rows.length - 1; i += 1) {
             const r = txs.rows[i];
             const date = parseDate(r[0]);
-            const details = r[1];
+            const payee = r[1];
             const amount = (r[2] != '') ? parseCurrency(r[2]) : parseCurrency(r[3]);
             transactions.push(new Transaction(
                 date,
                 account,
                 amount,
-                details,
-                (r[2] != '') ? Flow.DEBIT : Flow.CREDIT
+                payee,
+                (r[2] != '') ? Flow.Debit : Flow.Credit
             ));
         }
 
         return transactions;
     }
 
-    /**
-    * Called by ledgerize to check if the tab is on a statement page that can be
-    * collected.
-    *
-    * @return The name of the account being displayed in this tab, null otherwise.
-    */
+    /// Called by ledgerize to check if the tab is on a statement page that can be
+    /// collected.
+    ///
+    /// @return The name of the account being displayed in this tab, null otherwise.
     check(): Option<string> {
         let ops = document.getElementById('dernieresoperations');
         if (!ops) return null; // Not on a statement page.
